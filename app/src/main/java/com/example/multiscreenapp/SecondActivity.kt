76247f7d803b3b26411a62df7766c371d5d75110
Magzhan <11magzhan@gmail.com>
2024-02-27 13:22:35 +0600

    add viewBinding

--- app/src/main/java/com/example/multiscreenapp/SecondActivity.kt
+++ app/src/main/java/com/example/multiscreenapp/SecondActivity.kt
@@ -2,10 +2,15 @@ package com.example.multiscreenapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.multiscreenapp.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/multiscreenapp/ThirdActivity.kt
+++ app/src/main/java/com/example/multiscreenapp/ThirdActivity.kt
@@ -2,10 +2,15 @@ package com.example.multiscreenapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.multiscreenapp.databinding.ActivityThirdBinding
 
 class ThirdActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityThirdBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_third)
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
