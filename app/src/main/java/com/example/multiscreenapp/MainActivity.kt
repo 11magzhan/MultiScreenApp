f30736d85c6b6d8164ebd766f24905ebaee34c6d
Magzhan <11magzhan@gmail.com>
2024-02-27 14:28:59 +0600

    finish not works

--- app/src/main/java/com/example/multiscreenapp/MainActivity.kt
+++ app/src/main/java/com/example/multiscreenapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.multiscreenapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.multiscreenapp.databinding.ActivityMainBinding
@@ -12,5 +13,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val btnForward = binding.btnGo
+        btnForward.setOnClickListener { navigateForward() }
+
+        val btnBack = binding.btnBack
+        btnBack.setOnClickListener { navigateBack() }
+    }
+
+    private fun navigateForward() {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateBack() {
+        finish()
     }
 }
--- app/src/main/java/com/example/multiscreenapp/ThirdActivity.kt
+++ app/src/main/java/com/example/multiscreenapp/ThirdActivity.kt
@@ -1,5 +1,6 @@
 package com.example.multiscreenapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.multiscreenapp.databinding.ActivityThirdBinding
@@ -12,5 +13,21 @@ class ThirdActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityThirdBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val btnForward = binding.btnGo
+        btnForward.setOnClickListener { navigateForward() }
+
+        val btnBack = binding.btnBack
+        btnBack.setOnClickListener { navigateBack() }
+    }
+
+    private fun navigateForward() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateBack() {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
     }
 }
