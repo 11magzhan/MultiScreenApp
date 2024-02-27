0b3812ee8ae33d979a43ae7a65a445ffffdd8a48
Magzhan <11magzhan@gmail.com>
2024-02-27 16:28:49 +0600

    lifecycle log add

--- app/src/main/java/com/example/multiscreenapp/MainActivity.kt
+++ app/src/main/java/com/example/multiscreenapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.multiscreenapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.multiscreenapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i("MainActivity", "onCreate")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -28,5 +30,31 @@ class MainActivity : AppCompatActivity() {
 
     private fun navigateBack() {
         finish()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainActivity", "onStart")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("MainActivity", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("MainActivity", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("MainActivity", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MainActivity", "onDestroy")
     }
 }
--- app/src/main/java/com/example/multiscreenapp/SecondActivity.kt
+++ app/src/main/java/com/example/multiscreenapp/SecondActivity.kt
@@ -3,6 +3,7 @@ package com.example.multiscreenapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.multiscreenapp.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
@@ -11,6 +12,7 @@ class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i("SecondActivity", "onCreate")
         binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -27,7 +29,32 @@ class SecondActivity : AppCompatActivity() {
     }
 
     private fun navigateBack() {
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
+        finish()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("SecondActivity", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("SecondActivity", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("SecondActivity", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("SecondActivity", "onStop"
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("SecondActivity", "onDestroy")
     }
 }
